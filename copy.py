b974359d986f45fce958d12787933be6f482e8c3
Erneste304 <ernestegisubizo83@gmail.com>
2025-05-02 16:52:38 +0000

    answer of third question

--- copy.py
+++
@@ -1,8 +0,0 @@
-It seems like you have pasted the same code twice. If you need help modifying or improving the code, could you clarify what changes or enhancements you'd like to make? For example:
-
-- Adding new functionality?
-- Improving error handling?
-- Refactoring for better readability?
-- Adding more test cases?
-
-Let me know how I can assist!
---
+++ task2-custom-function/text_formatter.py
@@ -0,0 +1,7 @@
+It seems like the code you provided in the user prompt is identical to the code in the current document. If you need help modifying or improving this code, please specify what changes or enhancements you'd like to make. For example:
+
+- Do you want to add new features?
+- Do you want to refactor the code for better readability or performance?
+- Do you need help with testing or debugging?
+
+Let me know how I can assist!
